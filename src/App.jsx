093ceee55dc8bd13b6edcb81902d1cabcca178fb
leotarditi <57666013+leotarditi@users.noreply.github.com>
2025-02-12 21:06:50 -0300

    Add useInfiniteQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AppRouter } from "./AppRouter"
 import { CartProvider } from "./contexts/cart"
 import { FiltersProvider } from "./contexts/filters"
 import { UserProvider } from "./contexts/user"
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <UserProvider>
-      <FiltersProvider>
-        <CartProvider>
-          <AppRouter />
-        </CartProvider>
-      </FiltersProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <FiltersProvider>
+          <CartProvider>
+            <AppRouter />
+          </CartProvider>
+        </FiltersProvider>
+      </UserProvider>
+    </QueryClientProvider>
   )
 }
 
